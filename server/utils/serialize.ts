deaadce5a4908603c2369861473bf19a5aeef0e6
Grave Break <admin@gravebreak.com>
2026-07-04 14:50:01 -0400

    created new serialize util for creating/reading/writing json files.

---
+++ server/utils/serialize.ts
@@ -0,0 +1,54 @@
+import { readFileSync, writeFileSync, existsSync, mkdirSync } from "fs";
+import { mkdir, writeFile } from "fs/promises";
+import { dirname } from "path";
+
+import { AppError } from "./errors";
+
+export function existsFile(path: string): boolean {
+	return existsSync(path);
+}
+
+export function createJsonFile(path: string, defaultValue: unknown){
+	try {
+		mkdirSync(dirname(path), { recursive: true });
+		writeFileSync(path, JSON.stringify(defaultValue, null, 4), { flag: 'wx' });
+	}
+	catch (error: unknown) {
+		if (error instanceof Error) {
+			throw new AppError(`failed to create JSON file at ${path}: ${error.message}`, 'internal', 'warn');
+		}
+		else {
+			throw new AppError(`non error thrown while creating JSON file at ${path}: ${error}`, 'internal', 'error');
+		}
+	}
+}
+
+export function readJsonFile(path: string): unknown {
+	try {
+		const raw = readFileSync(path, 'utf-8');
+		return JSON.parse(raw);
+	}
+	catch (error: unknown) {
+		if (error instanceof Error) {
+			throw new AppError(`failed to read/parse JSON file at ${path}: ${error.message}`, 'internal', 'warn');
+		}
+		else {
+			throw new AppError(`non error thrown while reading JSON file at ${path}: ${error}`, 'internal', 'error');
+		}
+	}
+}
+
+export async function writeJsonFile(path: string, data: unknown){
+	try {
+		await mkdir(dirname(path), { recursive: true });
+		await writeFile(path, JSON.stringify(data, null, 4));
+	}
+	catch (error: unknown) {
+		if (error instanceof Error) {
+			throw new AppError(`failed to write JSON file at ${path}: ${error.message}`, 'internal', 'error');
+		}
+		else {
+			throw new AppError(`non error thrown while writing JSON file at ${path}: ${error}`, 'internal', 'error');
+		}
+	}
+}

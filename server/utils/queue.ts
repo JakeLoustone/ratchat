8b631287d5c12d23cf7826d6fbb2635937269dbb
Grave Break <admin@gravebreak.com>
2026-07-04 15:36:24 -0400

    created new queue.ts util to handle async serialized saves

---
+++ server/utils/queue.ts
@@ -0,0 +1,14 @@
+import { handleError } from "./errors";
+
+export function createSaveQueue(save: () => Promise<void>) {
+	let pending: Promise<void> = Promise.resolve();
+
+	return {
+		chain(){
+			pending = pending.then(save)
+            .catch((error: unknown) => {
+				handleError(error, `Save Queue: ${save.name}`);
+			});
+		},
+	};
+}
